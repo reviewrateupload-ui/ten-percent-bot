f4ff70db3c116153a46c2da1429301350fe4d77b
reviewrateupload-ui <review.rate.upload@gmail.com>
2026-06-05 23:37:15 +0300

    BTC trend test

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import os
 import requests
+import pandas as pd
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-coins = [
-    "BTCUSDT",
-    "ETHUSDT",
-    "SOLUSDT",
-    "XRPUSDT",
-    "SUIUSDT"
-]
+url = "https://api.mexc.com/api/v3/klines?symbol=BTCUSDT&interval=15m&limit=50"
 
-message = "📊 Live Prices\n\n"
+response = requests.get(url)
 
-for coin in coins:
-    response = requests.get(
-        f"https://api.mexc.com/api/v3/ticker/price?symbol={coin}"
-    )
+candles = response.json()
 
-    price = response.json()["price"]
+df = pd.DataFrame(candles)
 
-    message += f"{coin}: {price}\n"
+df[4] = df[4].astype(float)
+
+ema20 = df[4].ewm(span=20).mean().iloc[-1]
+
+current_price = df[4].iloc[-1]
+
+if current_price > ema20:
+    trend = "🟢 BTC Trend: Bullish"
+else:
+    trend = "🔴 BTC Trend: Bearish"
 
 telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -29,8 +30,8 @@ requests.post(
     telegram_url,
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": trend
     }
 )
 
-print(message)
+print(trend)
