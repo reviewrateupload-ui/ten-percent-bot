1b1d265e8a226782af421b4bc50962d745340378
reviewrateupload-ui <review.rate.upload@gmail.com>
2026-06-05 22:55:52 +0300

    Connect Telegram

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Ten Percent Bot Started")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "🚀 Ten Percent Bot connected successfully"
+    }
+)
+
+print("Message sent")
